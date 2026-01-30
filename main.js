2cee3cdb0e75af782bcdcda3333ac1c65c2d9a95
unknown <alinanumanova555@gmail.com>
2026-01-30 15:44:18 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+new fullpage('#fullpage', {
+  autoScrolling: true,
+  navigation: true,       
+  navigationPosition: 'right',
+  scrollOverflow: true,
+  anchors: ['first', 'second', 'third', 'fourth'],
+});
